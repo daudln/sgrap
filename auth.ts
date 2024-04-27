7614c24dffb8407e5d469fc5e7acca76118a67f5
daudln <daudnamayala@gmail.com>
2024-04-27 15:07:25 +0300

    ✨ Fix GET and POST auth handlers

--- auth.ts
+++ auth.ts
@@ -4,12 +4,7 @@ import prisma from "./lib/utils";
 import authconfig from "./auth.config";
 import { getUserById } from "./server/auth/lib";
 
-export const {
-  handlers: { GET, POST },
-  auth,
-  signIn,
-  signOut,
-} = NextAuth({
+export const { handlers, auth, signIn, signOut } = NextAuth({
   adapter: PrismaAdapter(prisma),
   session: {
     strategy: "jwt",
